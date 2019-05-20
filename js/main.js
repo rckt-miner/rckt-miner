84993df1ff3b78043949b151a16b23cfcc96d85d
rckt-miner <50653084+rckt-miner@users.noreply.github.com>
2019-05-20 12:34:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@
 	
 	var access_token = url.searchParams.get("access_token");
 	var api_result = url.searchParams.get("api_result");
-	var ref = url.searchParams.get("user_id");
+	var ref = url.searchParams.get("group_id");
 	
 	var user_data = "";
 	var randomstring = "";
